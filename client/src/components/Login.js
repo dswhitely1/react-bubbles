ee337d0c5d60fe6f25a2fcb8d442395ea1196362
Donald Whitely <dswhitely1@gmail.com>
2019-11-14 12:45:40 -0500

    Receiving Token from the API

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, {useCallback, useState} from "react";
+import axios from 'axios';
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+    const [values, setValues] = useState({
+        username: '',
+        password: ''
+    });
+
+    function handleChange({target: {name, value}}) {
+        setValues({...values, [name]: value});
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        axios
+            .post('http://localhost:5000/api/login', values)
+            .then(res => console.log(res.data))
+            .catch(err => console.log(err.response));
+    }
+    // make a post request to retrieve a token from the api
+    // when you have handled the token, navigate to the BubblePage route
+    return (
+        <>
+            <h1>Welcome to the Bubble App!</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Username: <input name='username' value={values.username} onChange={handleChange}/></label>
+                <label>Password: <input type="password" name='password' value={values.password}
+                                        onChange={handleChange}/></label>
+                <button type='submit'>Submit</button>
+            </form>
+        </>
+    );
 };
 
 export default Login;
